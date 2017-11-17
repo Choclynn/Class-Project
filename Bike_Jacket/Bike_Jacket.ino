ca5d543226e444b5ce66fa028f85f330dda6ce4d
Quinten Ruckaber <quinten.d@sasktel.net>
2017-11-16 19:38:41 -0600

    added an overall statement
    briefly explained the purpose of sketch, likely will improve later

--- Bike_Jacket/Bike_Jacket.ino
+++ Bike_Jacket/Bike_Jacket.ino
@@ -1,4 +1,12 @@
-void setup() {
+/*
+ This code is ment to work a jacket with 2 different 
+ turn indicators and brake lights to simulate that of
+ a car. Each inidicator has it's own button that controls
+ it by pushing it. There is a third button that controls 
+ the brake lights, however this light must be held in order for
+ the brake lights to be on. 
+ */
+ void setup() {
   // put your setup code here, to run once:
 
 }

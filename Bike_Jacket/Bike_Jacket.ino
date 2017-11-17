4716b64ac8894510f2fa3263fe736deb9b6a7ef1
Quinten Ruckaber <quinten.d@sasktel.net>
2017-11-16 20:42:03 -0600

    good chunk of code made
    all constants and most variables have been added. Likely will go a different route with the button and the way it controls the lights. Trying to figure that out now.

--- Bike_Jacket/Bike_Jacket.ino
+++ Bike_Jacket/Bike_Jacket.ino
@@ -6,12 +6,85 @@
  the brake lights, however this light must be held in order for
  the brake lights to be on. 
  */
- void setup() {
-  // put your setup code here, to run once:
+ //constants to be used in the program
+ const int right1 = 5;  //
+ const int right2 = 4;  //these four variables are meant to control
+ const int right3 = 3;  //the right turn signal. Right1, 2 and 3
+ const int right4 = 2;  //all control 2 lights
 
-}
+ const int left1 = 6; //
+ const int left2 = 7; //these four variables are meant to control
+ const int left3 = 8; //the left turn signal. Left1, 2 and 3
+ const int left4 = 9; //all control 2 lights
 
-void loop() {
-  // put your main code here, to run repeatedly:
+ const int rLED = 13; //controls LED that helps indicate the right button pushed
+ const int lLED = 12; //controls LED that helps indicate the left button pushed
 
+ const int stopLED = 11; //controls the brake light LEDs
+
+ const int rButton = A5;  //button that controls right signal
+ const int lButton = A4;  //button that controls left signal
+ const int stopButton = A3;  //button that controls stop lights
+
+ //variables to be used in the program
+ int lastRState = 0;
+ int currRState = 0;
+ int lastLState = 0;
+ int currLstate = 0;
+ int stopState = 0;
+ int wait = 75;
+ 
+ void setup() 
+ {
+  //initialize all LEDs as outputs
+  pinMode(right1, OUTPUT);
+  pinMode(right2, OUTPUT);
+  pinMode(right3, OUTPUT);
+  pinMode(right4, OUTPUT);
+  pinMode(left1, OUTPUT);
+  pinMode(left2, OUTPUT);
+  pinMode(left3, OUTPUT);
+  pinMode(left4, OUTPUT);
+  pinMode(rLED, OUTPUT);
+  pinMode(lLED, OUTPUT); 
+  pinMode(stopLED, OUTPUT);
+ }
+
+void loop() 
+{
+  currRState = analogRead(rButton);
+  if (currRState == 0) //if button is pressed
+  {
+   digitalWrite(rLED, HIGH);
+   digitalWrite(right1, HIGH);
+   delay(wait);
+   digitalWrite(right1, LOW);
+   delay(wait);
+   digitalWrite(rLED, LOW);
+   digitalWrite(right2, HIGH);
+   delay(wait);
+   digitalWrite(right2, LOW);
+   delay(wait);
+   digitalWrite(rLED, HIGH);
+   digitalWrite(right3, HIGH);
+   delay(wait);
+   digitalWrite(right3, LOW);
+   delay(wait);
+   digitalWrite(right4, HIGH);
+   delay(wait);
+   digitalWrite(right4, LOW);
+   digitalWrite(rLED, LOW);
+   delay(wait);
+  }
+
+  else
+  {
+    digitalWrite(right1, LOW);
+   digitalWrite(right2, LOW);
+   digitalWrite(right3, LOW);
+   digitalWrite(right4, LOW);
+   digitalWrite(rLED, LOW);
+  }
+ 
+  
 }
